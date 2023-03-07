14406c1220be2b8f2d6c13667ea61e508b9acb86
Nickolay Petrych <npetrych@alfabank.ru>
2023-03-07 21:59:24 +0500

    init commit

---
+++ src/test/java/Nick/Petrych/Sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package Nick.Petrych.Sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
